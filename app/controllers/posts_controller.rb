5d055448c329d22501f1efb2ae80f9bf38f6385b
Hillary Okello <hillariouskelly@outlook.com>
2021-01-28 12:50:12 +0300

    restricted access to new and create methods to users who are signed in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only:[:new, :create]
   before_action :set_post, only: %i[ show edit update destroy ]
 
   # GET /posts or /posts.json
