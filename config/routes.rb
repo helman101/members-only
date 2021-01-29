9f337126324c722478eec0727266db195116dd6c
Hillary Okello <hillariouskelly@outlook.com>
2021-01-29 11:25:17 +0300

    permit name and username in registations_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :posts, only:[:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "posts#index"
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 end
